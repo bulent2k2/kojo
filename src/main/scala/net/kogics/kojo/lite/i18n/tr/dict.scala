4a1f52cad37e103d8999c9033e88c7368735531b
Ben Bulent Basaran <ben@scala.org>
2025-01-16 18:49:46 -0500

    version info before releasing Koco (Kojo-TR)

--- src/main/scala/net/kogics/kojo/lite/i18n/tr/dict.scala
+++ src/main/scala/net/kogics/kojo/lite/i18n/tr/dict.scala
@@ -16,8 +16,8 @@
  */
 /*
 Koco version tag for timestamping purposes:
-  val KojoRevision = "r9-tr-1"
-  val KojoBuildDate = "14 Aralık 2024"
+  val KojoRevision = "r9-tr-2"
+  val KojoBuildDate = "17 Ocak 2025"
 Ref Kojo version: ../../Versions.scala
   I do not check in to Version.scala.
   Makes it hard to sync by fork with Lalit's base.
